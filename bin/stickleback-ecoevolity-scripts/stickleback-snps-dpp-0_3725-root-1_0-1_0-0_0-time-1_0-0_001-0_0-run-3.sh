f695ce44c926868c1893b5d86850d91340053b75
Jamie R. Oaks <joaks1@gmail.com>
2018-12-09 11:06:11 -0600

    Adding new qsub scripts for stickleback analyses.

---
+++ bin/stickleback-ecoevolity-scripts/stickleback-snps-dpp-0_3725-root-1_0-1_0-0_0-time-1_0-0_001-0_0-run-3.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    module load gcc/5.3.0
+fi
+
+run_number=3
+seed=602482080
+config_prefix="stickleback-snps-dpp-0_3725-root-1_0-1_0-0_0-time-1_0-0_001-0_0"
+
+output_dir="../../stickleback-ecoevolity-output"
+
+if [ ! -d "$output_dir" ]
+then
+    mkdir "$output_dir"
+fi
+
+config_path="../../configs/${config_prefix}.yml"
+prefix="${output_dir}/run-${run_number}-"
+
+ecoevolity --seed "$seed"  --prefix "$prefix" --relax-missing-sites --relax-constant-sites --relax-triallelic-sites "$config_path" 1>"${prefix}${config_prefix}.out" 2>&1
