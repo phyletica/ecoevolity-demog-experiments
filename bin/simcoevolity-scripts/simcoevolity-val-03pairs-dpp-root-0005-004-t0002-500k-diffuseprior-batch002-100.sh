035fc1d97ecd931cca6f08a25422568e2fb9d435
Jamie R. Oaks <joaks1@gmail.com>
2019-04-26 14:06:53 -0500

    Adding simcoevolity scripts.
    Adding scripts for 400 more reps of the diffuse-prior analyses.  Also,
    adding scripts for diffuse-prior analyses with pairs of populations
    (divergences rather than pop-size increases).

---
+++ bin/simcoevolity-scripts/simcoevolity-val-03pairs-dpp-root-0005-004-t0002-500k-diffuseprior-batch002-100.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+simname="03pairs-dpp-root-0005-004-t0002-500k"
+cfgpath="../../configs/config-${simname}.yml"
+priorcfgpath="../../configs/config-03pairs-diffuse-prior-500k.yml"
+outputdir="../../simulations/validation/${simname}-diffuseprior/batch002"
+rngseed=958491663
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -p "$priorcfgpath" -n "$nreps" -o "$outputdir" "$cfgpath"
---
+++ bin/simcoevolity-scripts/simcoevolity-val-03pops-dpp-root-0005-004-t0002-500k-diffuseprior-batch002-100.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+simname="03pops-dpp-root-0005-004-t0002-500k"
+cfgpath="../../configs/config-${simname}.yml"
+priorcfgpath="../../configs/config-03pops-diffuse-prior-500k.yml"
+outputdir="../../simulations/validation/${simname}-diffuseprior/batch002"
+rngseed=958491663
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -p "$priorcfgpath" -n "$nreps" -o "$outputdir" "$cfgpath"
+
+
+simname="03pops-dpp-root-0005-004-3_8-t0002-500k"
+cfgpath="../../configs/config-${simname}.yml"
+outputdir="../../simulations/validation/${simname}-diffuseprior/batch002"
+rngseed=526712515
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -p "$priorcfgpath" -n "$nreps" -o "$outputdir" "$cfgpath"
